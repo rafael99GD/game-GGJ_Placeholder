35a3d7b4da1551f0de045893c0785642e1258d2e
Sauroun <106895326+Sauroun@users.noreply.github.com>
2026-02-01 14:16:11 +0100

    aaaa

---
+++ Assets/Devs/Sauron/Scripts/Interacciones/PlayerInteractionIndicator.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerInteractionIndicator : MonoBehaviour
+{
+    [Header("Visual")]
+    [SerializeField] private GameObject indicadorE;
+
+    [Header("Detección")]
+    [SerializeField] private float radioDeteccion = 1.2f;
+    [SerializeField] private LayerMask capaInteractuable;
+
+    private bool hayInteractuableCerca;
+
+    private void Awake()
+    {
+        if (indicadorE != null)
+            indicadorE.SetActive(false);
+    }
+
+    private void Update()
+    {
+        DetectarInteractuable();
+        ActualizarIndicador();
+    }
+
+    private void DetectarInteractuable()
+    {
+        hayInteractuableCerca = Physics.CheckSphere(
+    transform.position,
+    radioDeteccion,
+    capaInteractuable,
+    QueryTriggerInteraction.Collide
+);
+
+    }
+
+    private void ActualizarIndicador()
+    {
+        if (indicadorE == null) return;
+
+        if (indicadorE.activeSelf != hayInteractuableCerca)
+            indicadorE.SetActive(hayInteractuableCerca);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radioDeteccion);
+    }
+}
